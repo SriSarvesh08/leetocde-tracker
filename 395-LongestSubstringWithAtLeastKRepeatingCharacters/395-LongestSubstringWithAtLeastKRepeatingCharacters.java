63efc19383487453f979370f7c68c8a4f7544af1
Sri Sarvesh B <srisarvesh.b78@gmail.com>
2026-07-08 15:35:10 +0530

    Create 395-LongestSubstringWithAtLeastKRepeatingCharacters

---
+++ 395-LongestSubstringWithAtLeastKRepeatingCharacters/395-LongestSubstringWithAtLeastKRepeatingCharacters.java
@@ -0,0 +1,19 @@
+// Last updated: 7/8/2026, 3:35:07 PM
+class Solution {
+    public int longestSubstring(String s, int k) {
+        int n = s.length();
+        int[] freq = new int[26];
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (freq[s.charAt(i) - 'a'] < k) {
+                int left = longestSubstring(s.substring(0, i), k);
+                int right = longestSubstring(s.substring(i + 1), k);
+                return Math.max(left, right);
+            }
+        }
+        return n;
+    }
+}
