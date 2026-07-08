dfb1119400abe8987b06c3764bbe63c94c6618a1
Sri Sarvesh B <srisarvesh.b78@gmail.com>
2026-07-08 15:35:00 +0530

    Create 457-CircularArrayLoop

---
+++ 457-CircularArrayLoop/457-CircularArrayLoop.java
@@ -0,0 +1,32 @@
+// Last updated: 7/8/2026, 3:34:57 PM
+class Solution {
+    int n;
+    public boolean circularArrayLoop(int[] nums) {
+        n = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            Set<Integer> set = new HashSet<>();
+            boolean isForward=(nums[i]>=0);
+            if (dfs(i, nums, set,isForward)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean dfs(int si, int nums[], Set<Integer> set,boolean isForward) {
+        if(si<0 || si>=nums.length) return false;
+        if(nums[si]<0 && isForward==true) return false;
+        if(nums[si]>=0 && isForward==false) return false; 
+        int index=(nums[si]+si)%n;
+        if(index<0){
+            index=n+index;
+        }
+        if(index==si) return false;
+        if(set.contains(index)) return true;
+        set.add(index);
+        if(dfs(index,nums,set,isForward)){
+            return true;
+        }
+        return false;
+    }
+}
