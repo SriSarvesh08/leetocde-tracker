6b1b22de4eee9543552ba251843e0569c2a2737f
Sri Sarvesh B <srisarvesh.b78@gmail.com>
2026-07-08 15:39:48 +0530

    Create 21-MergeTwoSortedLists

---
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
@@ -0,0 +1,13 @@
+// Last updated: 7/8/2026, 3:39:45 PM
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy=new ListNode(0),cur=dummy;
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val){cur.next=l1;l1=l1.next;}
+            else{cur.next=l2;l2=l2.next;}
+            cur=cur.next;
+        }
+        cur.next=(l1!=null)?l1:l2;
+        return dummy.next;
+    }
+}
